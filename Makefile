4aea521eec7a4a1de2344c35dfbaffe09cd9c4b0
Katsuyuki Toki <katuyuki.toki@gmail.com>
2021-11-29 22:00:56 +0900

    1st commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+run:
+	gradlew tasks bootRun
+
+help:
+	gradlew tasks --all
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO inquiry(name, email, contents, created)
+VALUES ('Ethan', 'sample@sample.com', 'Hello', '2021-11-27 16:58:00');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES ('Emma', 'sample2@sample2.com', 'Good Morning', '2021-11-27 16:58:00');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES ('William', 'sample3@sample3.com', 'Good bye', '2021-11-27 16:58:00');
